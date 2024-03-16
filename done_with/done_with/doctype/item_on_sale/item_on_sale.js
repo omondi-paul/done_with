73e1aa1207eccc9d74deb79ab91d7ccdf01a2c4d
omondi-paul <omondipaulok@gmail.com>
2024-03-16 10:52:15 +0300

    Some server script code on Item doctype

--- done_with/done_with/doctype/item_on_sale/item_on_sale.js
+++ done_with/done_with/doctype/item_on_sale/item_on_sale.js
@@ -1,8 +1,31 @@
 // Copyright (c) 2024, Polito and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Item on Sale', {
-	// refresh: function(frm) {
+// frappe.ui.form.on('Item on Sale', {
+// 	// refresh: function(frm) {
 
-	// }
+// 	// }
+// });
+
+frappe.ui.form.on('Item on Sale', {
+    offer_price: function(frm){
+     
+        frappe.call({
+            method: 'done_with.services.rest.list_item',
+            args: {
+                "email": email
+            },
+            callback: function (r) {
+                if (r.message) {
+                    // Get the button element by its ID
+    
+                    // Add a click event listener to the button
+                    offer_price.on("click", function() {
+                        // Redirect to the offer price page
+                        window.location.href = '/desk#List/Offer'; 
+                    });
+                }
+            }
+        });
+    },
 });
