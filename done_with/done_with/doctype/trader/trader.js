f538a1d1d23dea47ac4bdfacc48e00f1b5490b98
omondi-paul <omondipaulok@gmail.com>
2024-03-17 20:38:24 +0300

    more server scripts

--- done_with/done_with/doctype/trader/trader.js
+++ done_with/done_with/doctype/trader/trader.js
@@ -2,10 +2,45 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Trader', {
-	// validate: function(frm) {
+    onload: function (frm) {
+        frm.set_query('make_bid', 'listed_item', () => {
+            return {
+                filters: {
+                    "status": "Pending"
+                }
+            }
+        })
+      
+    },
+    refresh: function(frm) {
+        // Add a custom event handler for the "Bid" button click
+        // frm.fields_dict.check_items.$input.on('click', function() {
+        //     // Redirect to 'Make Bid' doctype
+        //     frappe.set_route('Form', 'Item on Sale');
+        // });
+        frappe.call({
+            method: 'done_with.services.rest.listed_items',
+            args: {
+            },
+            callback: function (r) {
+                console.log(r.message)    
+            }
+        });
+        frappe.call({
+            method: 'done_with.services.rest.received_bids',
+            args: {
+            },
+            callback: function (r) {
+                console.log(r.message) 
+            }
+        });
+    },
+    
+    // validate: function(frm) {
 		
 
-	// }
+	// },
+    
 });
 
 
@@ -37,7 +72,59 @@ frappe.ui.form.on('List Item', {
 		
 
     },
+
 });
+
+frappe.ui.form.on('Offer Request', {
+    item(frm, cdt, cdn) {
+        let row = locals[cdt][cdn];
+        let email = frm.doc.trader_email;
+        let contact=frm.doc.trader_contact;
+        let item_name=row.item
+       
+     
+        frappe.call({
+            method: 'done_with.services.rest.bidding_form',
+            args: {
+                "email": email,
+                "contact":contact
+            },
+            callback: function (r) {
+                if (r.message) {
+                    frappe.model.set_value(cdt, cdn, {
+                        bidder_email: r.message[0]
+					
+                    });
+					frappe.model.set_value(cdt, cdn, {
+                        contact: r.message[1]
+                    });
+
+                }
+            }
+        });
+        frappe.call({
+            method: 'done_with.services.rest.check_bidder',
+            args: {
+                "email": email,
+                "item_name":item_name
+            },
+            callback: function (r) {
+                if (r.message) {
+                    frappe.msgprint('You can\'t bid on your own item.');
+                    frappe.validate = false;
+                }
+                else {
+                    frappe.validate = true;
+                }
+            }
+        });
+		
+
+    },
+
+
+});
+
 
     
 
--- done_with/services/rest.py
+++ done_with/services/rest.py
@@ -22,3 +22,85 @@ class Item_on_Sale():
 def list_item(email):
     today = datetime.now().date()
     return email,today
+
+
+@frappe.whitelist(allow_guest=True)
+def listed_items():
+    items=frappe.db.get_all("List Item",filters={"status":"Available"},fields={"name","item_name","parent","condition","price","seller_id","date_listed","category","status","about_item","image"})
+    if items:
+        for item in items:
+            existing_item=frappe.db.get_all("Listed Item",{"name":item.name},{'name','item_name'})
+            if existing_item:
+                listedItem=frappe.get_doc("Listed Item",existing_item[0].name)
+                listedItem.item_name=item.item_name
+                listedItem.condition=item.condition #if item.condition else None
+                listedItem.price=item.price# if item.price else None
+                listedItem.date_listed=item.date_listed #if item.date_listed else None
+                listedItem.seller_id=item.seller_id #if item.seller_id else None
+                listedItem.category=item.category #if item.category else None
+                listedItem.status=item.status #if item.status else None
+                listedItem.about_item=item.about_item  #if item.about_item else None
+                listedItem.image=item.image # if item.image #else None
+                listedItem.save()
+            else:
+                listedItem=frappe.get_doc({
+                    "doctype": "Listed Item",
+                    "name":item.name,
+                    "item_name":item.item_name,
+                    "price":item.price,
+                    "condition": item.condition,
+                    "date_listed":item.date_listed,
+                    "seller_id":item.seller_id,
+                    "category":item.category,
+                    "status":item.status,
+                    "about_item":item.about_item,
+                    "image":item.image
+
+                })
+                listedItem.insert()
+            frappe.db.commit()
+
+
+    return 
+
+@frappe.whitelist(allow_guest=True)
+def bidding_form(email,contact):
+    return email,contact
+@frappe.whitelist(allow_guest=True)
+def check_bidder(email,item_name):
+    bidder=frappe.db.get_all("Listed Item",{"name":item_name,"seller_id":email},{"seller_id"})
+    if bidder and bidder[0].seller_id==email:
+        check=1
+    else: check=None
+
+    return check
+
+
+@frappe.whitelist(allow_guest=True)
+def received_bids():
+    offers=frappe.db.get_all("Offer Request",{},{"name","item","price_offer","bidder_email","contact","additional_contact"})
+    if offers:
+        for offer in offers:
+            existing_bid=frappe.db.get_all("Received Bid",{"name":offer.name},{"name","item_id"})
+    
+            items=frappe.db.get_all("List Item",filters={"name":offer.item},fields={"item_name","parent","condition","price","seller_id","date_listed","category","status","about_item","image"})
+            if existing_bid:
+                bid=frappe.get_doc("Received Bid",existing_bid[0].name)
+                bid.parent=items[0].parent
+                bid.seller_id=items[0].parent
+                bid.item_id=offer.item
+                bid.bidder=offer.bidder_email
+                bid.save()
+            else:
+                bid=frappe.get_doc({
+                    "doctype": "Received Bid",
+                    "name":offer.name,
+                    "parent":items[0].parent,
+                    "seller_id":items[0].parent,
+                    "item_id":offer.item,
+                    "bidder":offer.bidder_email,
+                })
+                bid.insert()
+            frappe.db.commit()
+
+    return "success"
